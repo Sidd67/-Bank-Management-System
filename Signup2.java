e8278d0ba8a8197403c4787bc474a345a06e7045
Sidd67 <Siddharthkeshari1234@gmail.com>
2025-04-12 18:39:00 +0530

    Add files via upload

---
+++ Signup2.java
@@ -0,0 +1,14 @@
+package ASimulatorSystem;
+
+public class Signup2 {
+
+    public Signup2(String first) {
+        
+    }
+
+    public void setVisible(boolean b) {
+       
+        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+    }
+
+}
